047bd612338b892ef54aead4382512ec60714be6
abr-ya <gamer05@list.ru>
2024-07-25 15:40:43 +0300

    feat: Add RemoteComponent to Home Page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,18 @@
+import { Suspense, lazy } from "react";
 import styles from "./home.module.css";
+import { Loader } from "@cars/uikit";
 
 const Home = () => {
+  const RemoteComponent = lazy(() => import("remote2/Test"));
+  console.log("RemoteComponent:", RemoteComponent);
+
   return (
     <div className={styles.homeContainer}>
       <h1>Hello, Styled React 2024-05!!!</h1>
       <h2>ENV test: {process.env.TEST}</h2>
+      <Suspense fallback={<Loader size="lg" />}>
+        <RemoteComponent />
+      </Suspense>
     </div>
   );
 };
